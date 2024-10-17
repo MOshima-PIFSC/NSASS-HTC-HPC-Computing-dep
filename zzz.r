f95ae3b783eeb2b6aaae6ba03373149a9b55c500
MOshima-PIFSC <megumi.oshima@noaa.gov>
2024-10-17 10:39:40 -1000

    Built site for gh-pages

--- zzz.r
+++
@@ -1,6 +0,0 @@
-# libraries that are needed but aren't referenced in the code
-# needed to parse package dependencies within Quarto files
-library(yaml)
-# below libraries are only needed for VSCode-R functionality
-library(rlang)
-library(jsonlite)
