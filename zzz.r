3d508fbb220377b6f69f84a8c90fa25bed08efa8
MOshima-PIFSC <megumi.oshima@noaa.gov>
2024-10-17 10:39:17 -1000

    Initializing gh-pages branch

---
+++ zzz.r
@@ -0,0 +1,6 @@
+# libraries that are needed but aren't referenced in the code
+# needed to parse package dependencies within Quarto files
+library(yaml)
+# below libraries are only needed for VSCode-R functionality
+library(rlang)
+library(jsonlite)
